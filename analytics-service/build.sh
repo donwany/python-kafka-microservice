21d03a23cc1d3c95a603f4e687ada9d3e53a3e5b
Rokas <94966788+Rokiis1@users.noreply.github.com>
2023-01-04 17:10:09 +0200

    Update build.sh

--- analytics-service/build.sh
+++ analytics-service/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login
-docker build --tag worldbosskafka/analytics:v1.0.0 . -f Dockerfile
-docker push worldbosskafka/analytics:v1.0.0
+docker build --tag rokiis1/analytics:v1.0.0 . -f Dockerfile
+docker push rokiis1/analytics:v1.0.0
