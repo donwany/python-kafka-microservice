475c3bba0b4418690341e2ed74dd1e25d36ab828
Rokas <94966788+Rokiis1@users.noreply.github.com>
2023-01-04 17:11:23 +0200

    Update build.sh

--- transaction-service/build.sh
+++ transaction-service/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login
-docker build --tag worldbosskafka/transactions:v1.0.0 . -f Dockerfile
-docker push worldbosskafka/transactions:v1.0.0
+docker build --tag rokiis1/transactions:v1.0.0 . -f Dockerfile
+docker push rokiis1/transactions:v1.0.0
